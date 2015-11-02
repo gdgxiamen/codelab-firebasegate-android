292328dc6f01a60eea12fc10a5934341725ef95f
Wind Wood <windwood@gmail.com>
2015-11-02 15:33:41 +0800

    由Android Studio生成的Empty Activity 项目名: Firebase Gate org.gdgxiamen.codelab

---
+++ app/src/androidTest/java/org/gdgxiamen/codelab/firebase_gate/ApplicationTest.java
@@ -0,0 +1,13 @@
+package org.gdgxiamen.codelab.firebase_gate;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
