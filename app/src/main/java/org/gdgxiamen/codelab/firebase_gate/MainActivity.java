5241472b1d8f87a394390e2db85e8ac9c2c170cd
Wind Wood <windwood@gmail.com>
2015-11-03 11:35:58 +0800

    创建按钮和标签的引用

--- app/src/main/java/org/gdgxiamen/codelab/firebase_gate/MainActivity.java
+++ app/src/main/java/org/gdgxiamen/codelab/firebase_gate/MainActivity.java
@@ -2,12 +2,22 @@ package org.gdgxiamen.codelab.firebase_gate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mTextNumber;
+    Button mButtonIn;
+    Button mButtonOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextNumber = (TextView) findViewById(R.id.textNumber);
+        mButtonIn = (Button) findViewById(R.id.buttonIn);
+        mButtonOut = (Button) findViewById(R.id.buttonOut);
     }
 }
